dbaca78afc1becf7114e7242917b3fe9a74c6a2b
tkomatsu <komats0315@gmail.com>
2020-12-03 08:33:36 +0900

    printfで時間付きで出力するように変更(一時的対処)

--- ft_print_dir.c
+++ ft_print_dir.c
@@ -6,17 +6,20 @@
 /*   By: tkomatsu <tkomatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 21:18:14 by tkomatsu          #+#    #+#             */
-/*   Updated: 2020/12/02 22:38:11 by tkomatsu         ###   ########.fr       */
+/*   Updated: 2020/12/03 08:13:54 by tkomatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_mini_ls.h"
 
-void	ft_print_dir(void *finfo)
+void	ft_print_dir(void *arg)
 {
-	t_info *tmp;
+	t_info *info;
 
-	tmp = (t_info*)finfo;
-	ft_putstr_fd(tmp->name, 1);
+	info = (t_info*)arg;
+	/*
+	ft_putstr_fd(info->name, 1);
 	ft_putchar_fd('\n', 1);
+	*/
+	printf("%15s	:%lu\n", info->name, info->stat.st_mtimespec.tv_sec);
 }
